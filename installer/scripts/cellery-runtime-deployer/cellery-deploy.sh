3f3e210a0d8a1550ecbaae27d5209eb8db23be9a
Deependra Ariyadewa <deependra@gmail.com>
2019-02-07 13:15:00 +0530

    Add istio k8s artifacts.

--- installer/scripts/cellery-runtime-deployer/cellery-deploy.sh
+++ installer/scripts/cellery-runtime-deployer/cellery-deploy.sh
@@ -300,9 +300,9 @@ local download_location=$1
 
 for param in "${!config_params[@]}"
 do
-    sed -i "s/$param/${config_params[$param]}/g" ${download_location}/apim-configs/pub-store/datasources/master-datasources.xml
-    sed -i "s/$param/${config_params[$param]}/g" ${download_location}/apim-configs/gw/datasources/master-datasources.xml
-    sed -i "s/$param/${config_params[$param]}/g" ${download_location}/sp-worker/conf/deployment.yaml
+    #sed -i "s/$param/${config_params[$param]}/g" ${download_location}/apim-configs/pub-store/datasources/master-datasources.xml
+    sed -i "s/$param/${config_params[$param]}/g" ${download_location}/distribution-master/installer/k8s-artefacts/global-apim/conf/datasources/master-datasources.xml
+    sed -i "s/$param/${config_params[$param]}/g" ${download_location}/distribution-master/installer/k8s-artefacts/observability/sp/conf/deployment.yaml
 done
 }
 
@@ -329,7 +329,7 @@ fi
 
 #Create the gw config maps
 kubectl create configmap gw-conf --from-file=${download_location}/distribution-master/installer/k8s-artefacts/global-apim/conf -n cellery-system
-kubectl create configmap gw-conf-datasources --from-file=${download_location}/distribution-master/installer/k8s-artefacts/global-apim/config/datasources/ -n cellery-system
+kubectl create configmap gw-conf-datasources --from-file=${download_location}/distribution-master/installer/k8s-artefacts/global-apim/conf/datasources/ -n cellery-system
 #Create KM config maps
 kubectl create configmap conf-identity --from-file=${download_location}/distribution-master/installer/k8s-artefacts/global-apim/conf/identity -n cellery-system
 kubectl create configmap apim-template --from-file=${download_location}/distribution-master/installer/k8s-artefacts/global-apim/conf/resources/api_templates -n cellery-system
