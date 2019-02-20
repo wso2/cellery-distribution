14d5a67705431d2482b1bcc067d77302efe5112c
Anjana Supun <anjanasupun05@gmail.com>
2019-02-20 17:08:40 +0530

    Decouple MicroGW from APIM

--- components/gateway/io.cellery.cell.gateway.initializer/src/main/java/io/cellery/cell/gateway/initializer/internals/ConfigManager.java
+++ components/gateway/io.cellery.cell.gateway.initializer/src/main/java/io/cellery/cell/gateway/initializer/internals/ConfigManager.java
@@ -15,9 +15,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package io.cellery.cell.gateway.initializer.internals;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cellery.cell.gateway.initializer.beans.controller.APIMConfig;
+import io.cellery.cell.gateway.initializer.utils.RequestProcessor;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.cellery.cell.gateway.initializer.beans.controller.Cell;
@@ -27,14 +31,18 @@ import io.cellery.cell.gateway.initializer.utils.Constants;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * Methods to read the configuration files
  */
 public class ConfigManager {
+
     private static final Logger log = LoggerFactory.getLogger(ConfigManager.class);
     private static volatile Cell cell = null;
     private static volatile RestConfig restConfig = null;
+    private static volatile APIMConfig apimConfig = null;
 
     /**
      * Initializes the Cell configuration
@@ -60,6 +68,20 @@ public class ConfigManager {
                     log.debug("Loading global configuration..");
                 }
                 restConfig = loadRESTConfig();
+            }
+        }
+    }
+
+    /**
+     * Initializes the Cell configuration
+     */
+    private static void apimInitialize() throws APIException {
+        synchronized (ConfigManager.class) {
+            if (apimConfig == null) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Loading apim configuration..");
+                }
+                loadApimConfig();
             }
         }
     }
@@ -101,6 +123,18 @@ public class ConfigManager {
     }
 
     /**
+     * Returns the whole configuration as a {@link APIMConfig} beans
+     *
+     * @return REST bean
+     */
+    public static APIMConfig getAPIMConfiguration() throws APIException {
+        if (apimConfig == null) {
+            apimInitialize();
+        }
+        return apimConfig;
+    }
+
+    /**
      * Load Cell configuration
      *
      * @return Cell Config
@@ -125,4 +159,90 @@ public class ConfigManager {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(new File(Constants.Utils.REST_CONFIGURATION_FILE_PATH), RestConfig.class);
     }
+
+    /**
+     * Load APIM configuration required for getting Access token
+     */
+    private static void loadApimConfig() throws APIException {
+        if (log.isDebugEnabled()) {
+            log.debug("Loading APIM Configs");
+        }
+        apimConfig = new APIMConfig();
+        String username = restConfig.getUsername();
+        String password = restConfig.getPassword();
+        byte[] message = (username + ":" + password).getBytes(StandardCharsets.UTF_8);
+        String userAuth = Base64.getEncoder().encodeToString(message);
+        generateClientIDSecret(userAuth);
+        generateAccessToken();
+    }
+
+    /**
+     * Generate Client ID and Client Secret.
+     *
+     * @param authHeader Authorization Header
+     * @throws APIException Throw an exception if any error occurred.
+     */
+    private static void generateClientIDSecret(String authHeader) throws APIException {
+        if (log.isDebugEnabled()) {
+            log.debug("Calling the dynamic client registration endpoint...");
+        }
+        RequestProcessor requestProcessor = new RequestProcessor();
+        String apimBaseURL = restConfig.getApimBaseUrl();
+        String applicationResponse = requestProcessor
+                .doPost(apimBaseURL + Constants.Utils.PATH_CLIENT_REGISTRATION + restConfig.getApiVersion() +
+                                Constants.Utils.PATH_REGISTER, Constants.Utils.CONTENT_TYPE_APPLICATION_JSON,
+                        Constants.Utils.CONTENT_TYPE_APPLICATION_JSON, Constants.Utils.BASIC + authHeader,
+                        restConfig.getRegisterPayload().toJSONString());
+
+        if (applicationResponse != null) {
+            JSONObject jsonObj = new JSONObject(applicationResponse);
+            apimConfig.setClientId(jsonObj.getString(Constants.Utils.CLIENT_ID));
+            apimConfig.setClientSecret(jsonObj.getString(Constants.Utils.CLIENT_SECRET));
+        }
+    }
+
+    /**
+     * Generate access tokens required to invoke RESTful APIs
+     *
+     * @throws APIException throw API Exception if an error occurred while generating an access token.
+     */
+    private static void generateAccessToken() throws APIException {
+        if (log.isDebugEnabled()) {
+            log.debug("Calling token endpoint to generate access tokens...");
+        }
+
+        String tokenPayload = Constants.Utils.TOKEN_PAYLOAD.replace("$USER", restConfig.getUsername())
+                .replace("$PASS", restConfig.getPassword());
+        apimConfig.setApiToken(getToken(tokenPayload));
+    }
+
+    /**
+     * Invoke Rest API to get token
+     *
+     * @param tokenPayload Post payload
+     * @return access token
+     * @throws APIException throw API Exception if an error occurred
+     */
+    private static String getToken(String tokenPayload) throws APIException {
+        RequestProcessor requestProcessor = new RequestProcessor();
+        String auth = getBase64EncodedClientIdAndSecret();
+        String apiCreateTokenResponse = requestProcessor
+                .doPost(restConfig.getTokenEndpoint(), Constants.Utils.CONTENT_TYPE_APPLICATION_URL_ENCODED,
+                        Constants.Utils.CONTENT_TYPE_APPLICATION_JSON, Constants.Utils.BASIC + auth, tokenPayload);
+
+        if (apiCreateTokenResponse != null) {
+            JSONObject jsonObj = new JSONObject(apiCreateTokenResponse);
+            return jsonObj.getString(Constants.Utils.ACCESS_TOKEN);
+        } else {
+            throw new APIException(
+                    "Error while generating the access token from token endpoint: " + restConfig.getTokenEndpoint());
+        }
+    }
+
+    private static String getBase64EncodedClientIdAndSecret() {
+        byte[] message = (apimConfig.getClientId() + ":" + apimConfig.getClientSecret()).getBytes(StandardCharsets.UTF_8);
+        return Base64.getEncoder().encodeToString(message);
+    }
+
 }
+
--- components/gateway/io.cellery.cell.gateway.initializer/src/main/java/io/cellery/cell/gateway/initializer/utils/Constants.java
+++ components/gateway/io.cellery.cell.gateway.initializer/src/main/java/io/cellery/cell/gateway/initializer/utils/Constants.java
@@ -72,6 +72,7 @@ public class Constants {
         public static final String URL = "url";
         public static final String SWAGGER = "swagger";
         public static final String ADDITIONAL_PROPERTIES = "additionalProperties";
+        public static final String NONE = "None";
     }
 
     /**
@@ -108,6 +109,8 @@ public class Constants {
         public static final String HOME_PATH = "/";
         public static final String UNZIP_FILE_PATH = "/unzip";
         public static final String MOUNT_FILE_PATH = "/target";
+        public static final String API_CONFIG_PATH = MOUNT_FILE_PATH + "/apiconfig.json";
+        public static final String SWAGGER_FOLDER = MOUNT_FILE_PATH +"/swagger/";
 
         // Token constants
         public static final String CLIENT_ID = "clientId";
