f41b1bd59d9fa6473723304dced6ed34ea224479
Anjana Supun <anjanasupun05@gmail.com>
2019-02-21 20:05:50 +0530

    Move cell gateway construction configs out of target volume

--- components/gateway/io.cellery.cell.gateway.initializer/src/main/java/io/cellery/cell/gateway/initializer/utils/Constants.java
+++ components/gateway/io.cellery.cell.gateway.initializer/src/main/java/io/cellery/cell/gateway/initializer/utils/Constants.java
@@ -109,8 +109,8 @@ public class Constants {
         public static final String HOME_PATH = "/";
         public static final String UNZIP_FILE_PATH = "/unzip";
         public static final String MOUNT_FILE_PATH = "/target";
-        public static final String API_CONFIG_PATH = MOUNT_FILE_PATH + "/apiconfig.json";
-        public static final String SWAGGER_FOLDER = MOUNT_FILE_PATH +"/swagger/";
+        public static final String API_CONFIG_PATH = "/apiconfig.json";
+        public static final String SWAGGER_FOLDER = "/swagger/";
 
         // Token constants
         public static final String CLIENT_ID = "clientId";
