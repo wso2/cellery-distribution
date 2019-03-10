2d4216ad7a2223698119835c50edd058f944107f
Deependra Ariyadewa <deependra@gmail.com>
2019-03-10 22:43:48 +0530

    Add VirtaulBox and Vagrant builder for the Cellery runtime.

---
+++ installer/packer/minimum-runtime-images/script/cleanup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -eu
+
+SSH_USER=${SSH_USERNAME:-vagrant}
+
+echo "==> Cleaning up tmp"
+rm -rf /tmp/*
+
+# Cleanup apt cache
+apt-get -y autoremove --purge
+apt-get -y clean
+
+echo "==> Installed packages"
+dpkg --get-selections | grep -v deinstall
+
+# Remove Bash history
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/${SSH_USER}/.bash_history
+
+echo "==> Clearing log files"
+find /var/log -type f -exec truncate --size=0 {} \;
+
+echo '==> Clear out swap and disable until reboot'
+set +e
+swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
+case "$?" in
+    2|0) ;;
+    *) exit 1 ;;
+esac
+set -e
+if [ "x${swapuuid}" != "x" ]; then
+    # Whiteout the swap partition to reduce box size
+    # Swap is disabled till reboot
+    swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
+    /sbin/swapoff "${swappart}"
+    dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
+    /sbin/mkswap -U "${swapuuid}" "${swappart}"
+fi
+
+# Zero out the free space to save space in the final image
+dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
+rm -f /EMPTY
+sync
+
+echo "==> Disk usage after cleanup"
+df -h
