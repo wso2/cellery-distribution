877e772a40b0e05f8dfda5a009aaa482e12e1deb
hasinthaindrajee <hasinthaindrajee@gmail.com>
2019-03-21 15:18:52 +0530

    update configs

--- installer/scripts/cellery-runtime-deployer/cellery-deploy.sh
+++ installer/scripts/cellery-runtime-deployer/cellery-deploy.sh
@@ -440,7 +440,8 @@ local release_version=$2
 #Create the IDP config maps
 kubectl create configmap identity-server-conf --from-file=${download_location}/distribution-${release_version}/installer/k8s-artefacts/global-idp/conf -n cellery-system
 kubectl create configmap identity-server-conf-datasources --from-file=${download_location}/distribution-${release_version}/installer/k8s-artefacts/global-idp/conf/datasources/ -n cellery-system
-#kubectl create configmap conf-identity --from-file=${download_location}/distribution-${release_version}/installer/k8s-artefacts/global-idp/conf/identity -n cellery-system
+kubectl create configmap identity-server-conf-identity
+--from-file=${download_location}/distribution-${release_version}/installer/k8s-artefacts/global-idp/conf/identity -n cellery-system
 kubectl create configmap identity-server-tomcat --from-file=${download_location}/distribution-${release_version}/installer/k8s-artefacts/global-idp/conf/tomcat -n cellery-system
 #kubectl create configmap identity-server-security --from-file=${download_location}/distribution-${release_version}/installer/k8s-artefacts/global-idp/conf/security -n cellery-system
 
