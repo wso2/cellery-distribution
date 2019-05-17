a5f4f8755b33d4a87d60dc1e9f8ee289bf49f89d
WSO2 Builder <jenkins-bot@wso2.com>
2019-05-17 10:29:28 +0000

    Update artifact versions for next development iteration

--- installer/packer/gapim-runtime-images/script/cellery.sh
+++ installer/packer/gapim-runtime-images/script/cellery.sh
@@ -18,8 +18,8 @@
 # ------------------------------------------------------------------------
 
 download_path=${DOWNLOAD_PATH:-tmp-cellery}
-release_version=0.2.1
-release_archive_version=v0.2.1
+release_version=master
+release_archive_version=master
 
 #Download k8s artifacts
 mkdir ${download_path}
